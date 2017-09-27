36b7ea705f2daf1aa7d2067edc8891a66349ff4f
NaveenDK <naveenkolambage@gmail.com>
2017-09-27 19:18:15 +1300

    sticky notes

---
+++ src/Board.js
@@ -0,0 +1,81 @@
+
+import './App.css'
+import Note from './Note'
+var React = require('react');
+var createReactClass = require('create-react-class');
+     var Board= React.createClass({
+                    propTypes:
+                    {
+                     count : function (props, propName){
+                     if (typeof props[propName]!="number"){
+                               return new Error("the count must be a number");
+                            }
+                     if(props[propName]>100){
+                               return new Error('Creating'+props[propName] +'notes is ridiculous')
+                            }
+                     }
+
+                    },
+                    nextId(){
+                      this.uniqueId=this.uniqueId || 0
+                      return this.uniqueId++
+                    },
+                    add(text){
+                     var notes=[
+                        ...this.state.notes,
+                        {
+                          id:this.nextId(),
+                          note: text
+                        }
+                      ]
+                      this.setState({notes})
+                    },
+
+                    getInitialState(){
+                        return{
+                           notes:[]
+                        }
+                    },
+                   
+                      update(newText,id){
+                      var notes = this.state.notes.map(
+                      note=>(note.id!==id)?
+                      note:{
+                            ...note,
+                            note: newText
+                            }
+
+                           )
+                           this.setState({notes}) 
+                       },
+
+                       remove(id){
+                       var notes=this.state.notes.filter(note=> note.id!==id)
+                       this.setState({notes})
+                       },
+                       eachNote(note){
+                          return <Note  key={note.id}
+                          id={note.id}onChange={this.update}
+                          onRemove={this.remove}>{note.note}</Note>
+                       },
+
+
+                      render(){
+                      return (<div className='board'>
+
+                          {this.state.notes.map(this.eachNote)}
+                          <button onClick={()=>this.add('New Note')}>
+                            +
+                          </button>
+                  
+                      </div>
+                      )
+                        
+                      }
+
+                    })
+
+        
+           
+
+export default Board
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './Board';
+
+import './index.css';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+	<Board count={50} />,
+	 document.getElementById('react-container'));
+registerServiceWorker();
